460d7b9470c3b3bb4bb8a242aef500a571e48125
Lucas Lopes <lucaslopesledok@gmail.com>
2021-05-15 21:02:34 -0300

    Project hr-config-server created

--- hr-config-server/src/test/java/br/com/devsuperior/hrconfigserver/HrConfigServerApplicationTests.java
+++ hr-config-server/src/test/java/br/com/devsuperior/hrconfigserver/HrConfigServerApplicationTests.java
@@ -1,10 +1,10 @@
-package br.com.devsuperior;
+package br.com.devsuperior.hrconfigserver;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class HrWorkerApplicationTests {
+class HrConfigServerApplicationTests {
 
 	@Test
 	void contextLoads() {
--- hr-worker/src/main/java/br/com/devsuperior/repositories/WorkerRepository.java
+++
@@ -1,9 +0,0 @@
-package br.com.devsuperior.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import br.com.devsuperior.entities.Worker;
-
-public interface WorkerRepository extends JpaRepository<Worker, Long>{
-
-}
